138295311b7bbb9e302f54c3705e96cd158ea78d
Cam Hung <camhung.phuong@sjsu.edu>
2026-02-05 19:16:59 -0800

    Exercise_2

--- app/src/main/java/edu/sjsu/android/project1CamPhuong/MainActivity.java
+++ app/src/main/java/edu/sjsu/android/project1CamPhuong/MainActivity.java
@@ -4,13 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.RadioGroup;
 import android.widget.SeekBar;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AlertDialog;
